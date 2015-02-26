575c16a36bb5c6f446785f1483a2269316ec4d11
Łukasz Mrozek <morskk@gmail.com>
2015-02-26 19:51:41 +0100

    Added application bootstrapper

---
+++ Mors.Words/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Mors.Words.CommandHandlers;
+using Mors.Words.Data.Commands;
+
+namespace Mors.Words
+{
+    public sealed class Bootstrapper
+    {
+        public void BootstrapCommands(CommandRegister commandRegister)
+        {
+            var commandHandler = new AddPolishGermanTranslationCommandHandler();
+            commandRegister(
+                typeof(AddPolishGermanTranslationCommand),
+                (cmd, epub) => commandHandler.Execute((AddPolishGermanTranslationCommand)cmd, epub));
+        }
+    }
+}
---
+++ Mors.Words/CommandRegister.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mors.Words
+{
+    public delegate void CommandRegister(Type commandType, Action<object, EventPublisher> commandHandler);
+}
